2ef29e7ebd003af02d93bf285a4e080f35b63fe4
Xiaoxun Zeng <seanzxx@me.com>
2026-09-05 11:49:15 -0700

    add proxyman@5.8.0

---
+++ Casks/proxyman@5.8.0.rb
@@ -0,0 +1,46 @@
+cask "proxyman@5.8.0" do
+  version "5.8.0,50800"
+  sha256 "ff3f81c0660dba805e0664f7254e90672df4dd78167e4abb47f2e5d99032f62e"
+
+  url "https://download.proxyman.com/#{version.csv.second}/Proxyman_#{version.csv.first}.dmg"
+  name "Proxyman"
+  desc "HTTP debugging proxy"
+  homepage "https://proxyman.com/"
+
+  livecheck do
+    skip "Version-pinned cask"
+  end
+
+  auto_updates true
+  depends_on macos: :big_sur
+
+  app "Proxyman.app"
+  binary "#{appdir}/Proxyman.app/Contents/MacOS/proxyman-cli"
+
+  uninstall_postflight_steps do
+    delete_keychain_certificates "Proxyman"
+  end
+
+  uninstall launchctl: "com.proxyman.NSProxy.HelperTool",
+            quit:      "com.proxyman.NSProxy",
+            delete:    "/Library/PrivilegedHelperTools/com.proxyman.NSProxy.HelperTool"
+
+  zap delete: "/Users/Shared/Proxyman",
+      trash:  [
+        "~/.proxyman*",
+        "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.proxyman.nsproxy.sfl*",
+        "~/Library/Application Support/com.proxyman",
+        "~/Library/Application Support/com.proxyman.NSProxy",
+        "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.proxyman.NSProxy",
+        "~/Library/Caches/com.proxyman.NSProxy",
+        "~/Library/Caches/Proxyman",
+        "~/Library/Cookies/com.proxyman.binarycookies",
+        "~/Library/Cookies/com.proxyman.NSProxy.binarycookies",
+        "~/Library/HTTPStorages/com.proxyman.NSProxy",
+        "~/Library/Preferences/com.proxyman.iconappmanager.userdefaults.plist",
+        "~/Library/Preferences/com.proxyman.NSProxy.plist",
+        "~/Library/Preferences/com.proxyman.plist",
+        "~/Library/Saved Application State/com.proxyman.NSProxy.savedState",
+        "~/Library/WebKit/com.proxyman.NSProxy",
+      ]
+end
